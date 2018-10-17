65a002e266f22e89223b1d87c091020fddcaa323
Maria218 <36620363+Maria218@users.noreply.github.com>
2018-10-17 21:17:15 +0200

    Updated script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.element', {
-  strings: ["an HTML expert", "a CSS specialist", "a soon to be fully fledged web developer", "hungry", "going to get snacks now", "gonna go eat for a bit", "going to be back soon, maybe", "gonna take a break and put all this on loop"],
+  strings: ["a JavaScript lover", "a fullstack developer", "a curious cat", "hungry", "going to get snacks now", "gonna go eat for a bit", "going to be back soon, maybe", "gonna take a break and put all this on loop"],
   typeSpeed: 60,
   backSpeed: 30,
   backDelay: 2000,
